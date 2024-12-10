82dff359a317a2a684fd8379dff239e124a19568
GeoRizkiNugroho <geosuryani@gmail.com>
2024-12-11 00:41:18 +0700

    baru

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public',  // Specify your output directory (e.g., 'dist' or 'public')
-        assetsDir: 'assets', // Optionally specify a subdirectory for assets
-    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
     ],
