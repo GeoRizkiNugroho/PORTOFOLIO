d30438f9fbe6ccb3a67f2aa6462eb070b7c33c96
GeoRizkiNugroho <geosuryani@gmail.com>
2024-12-10 23:36:46 +0700

    baru

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -27,7 +27,7 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   <!-- =======================================================
   * Template Name: Personal
   * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public', // Specifies where the build output should go
+        assetsDir: 'assets', // Optionally specify a subdirectory for assets
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
